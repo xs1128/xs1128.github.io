2f83d0da0855f22d5e09aed9de4d01e52f5f5820
Xinsheng Ooi <ooixinsheng12345@gmail.com>
2025-03-08 23:53:54 +0800

    Try push faulty .sh to check gthub CI's shellcehck again

--- missing-semester/pset8/wrong.sh
+++ missing-semester/pset8/wrong.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-# Write a bash function called pidwait that takes a pid 
-# and waits until the given process completes. 
+# Write a bash function called pidwait that takes a pid
+# and waits until the given process completes.
 # You should use sleep to avoid wasting CPU unnecessarily.
 
 pidwait() {
-    while kill - "$1" 2>/dev/null;
-    do
-    sleep 1
-    don
+    while kill - "$1" 2>/dev/null; d
+        sleep 1
+    done
     ls
 }
